332451e17c285b892d36b305de53b339fec66a7f
Eder Ramos <eder.ramosh@gmail.com>
2022-06-24 22:50:55 -0600

    Proyecto Completado

---
+++ README.md
@@ -0,0 +1,8 @@
+#Aplicacion de Bitacora de Viajes
+Es una aplicacion CRUD que usa Javascript usando Objetos y LocalStorage en donde podra hacer:
+1. Create (Crear)
+2. Read (Leer)
+3. Update (Actualizar)
+4. Delete (Borrar)
+
+El framework de CSS utilizado es Bootstrap (y tambien se uso CSS normal) y con ello se elaboro una aplicacion Responsiva y se implemento sus Validadores para el Form.
--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-const bitacora = [{
-    nombre: 'Eder Ramos',
-    telefono: '70458853',
-    tipoPago: 'Tarjeta',
-    monto: 7.20,
-    fecha: '2022-06-07',
-    hora: '21:00',
-    zona: 'San Salvador',
-}];
+let bitacora = [
+    {
+        nombre: 'Eder Ramos',
+        telefono: '70458853',
+        tipoPago: 'Tarjeta',
+        monto: 7.20,
+        fecha: '2022-06-07',
+        hora: '21:00',
+        zona: 'San Salvador',
+    },
+    {
+        nombre: 'Allison Estrada',
+        telefono: '77459652',
+        tipoPago: 'Efectivo',
+        monto: 15.35,
+        fecha: '2022-06-09',
+        hora: '19:00',
+        zona: 'Santa Tecla',
+    }
+];
 
 const formulario = document.getElementById('formulario');
 
@@ -16,6 +27,11 @@ formulario.addEventListener('submit', ingresarInformacion);
 
 // Funciones JavaScript
 function mostrarInformacion() {
+
+    let bitacoraTemporal = JSON.parse(localStorage.getItem('bitacora'));
+    if(bitacoraTemporal !== null){
+        bitacora = [...bitacoraTemporal];
+    }
 
     //Limpiamos la tabla
     document.getElementById('tablaBitacora').innerHTML = '';
@@ -30,9 +46,9 @@ function mostrarInformacion() {
             <td>${telefono}</td>
             <td>${tipoPago}</td>
             <td>$${monto}</td>
-            <td>${fecha}</td>
-            <td>${hora}</td>
-            <td>${zona}</td>
+            <td class="d-none d-md-table-cell">${fecha}</td>
+            <td class="d-none d-md-table-cell">${hora}</td>
+            <td class="d-none d-md-table-cell">${zona}</td>
             <td>
                 <button id="editarBoton" type="submit" onclick="editarBoton(${indice})" class="btn btn-outline-secondary"><svg height="25" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path></svg></button>
                 <button id="cerrarBoton" type="submit" onClick="eliminarViaje(${indice})" class="btn btn-outline-secondary"><svg height="25" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg></button>
@@ -56,13 +72,12 @@ function ingresarInformacion(e) {
 
     if(indice === '') {
         adicionarViaje( nombre, telefono, tipoPago, monto, fecha, hora, zona );
-        mostrarInformacion();
-        limpiarForm();
     } else {
         editarInformacion( indice, nombre, telefono, tipoPago, monto, fecha, hora, zona )
-        mostrarInformacion();
-        limpiarForm();
     }
+    localStorage.setItem('bitacora', JSON.stringify(bitacora));
+    mostrarInformacion();
+    limpiarForm();
 }
 
 function editarBoton(indice) {
@@ -99,21 +114,17 @@ function editarInformacion( indice, nombre, telefono, tipoPago, monto, fecha, ho
     bitacora[indice].fecha = fecha;
     bitacora[indice].hora = hora;
     bitacora[indice].zona = zona;
-    
 }
 
 function eliminarViaje(indice) {
     bitacora.splice(indice,1);
+    localStorage.setItem('bitacora', JSON.stringify(bitacora));
     mostrarInformacion();
+
 }
 
 function limpiarForm() {
     document.getElementById('formulario').reset();
 }
 
-
-//---------- test ----------
-adicionarViaje('Alexander', '22551111', 'Efectivo', 15.55, '2022-06-15', '15:30', 'Escalon');
-// editarViaje('Eustaquio', '22551111', 'efectivo', 15.55, '6/5/2022', '15:30', 'Merliot');
-// eliminarViaje(1);
 mostrarInformacion();
